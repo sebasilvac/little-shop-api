524261f4ed5cbf6a3584e004951d576346a608e3
Sebastián Silva <sebasilvac88@gmail.com>
2023-05-28 22:00:59 -0400

    check status

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -22,6 +22,13 @@ export class AuthController {
     return this.authService.login(loginUserDto);
   }
 
+  @Get('check-status')
+  @Auth()
+  checkAuthStatus(@GetUser() user: User) {
+    return this.authService.checkAuthStatus(user);
+  }
+
+
   @Get('private')
   @UseGuards(AuthGuard())
   privateRoute(
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -59,6 +59,13 @@ export class AuthService {
     };
   }
 
+  checkAuthStatus(user: User) {
+    return {
+      ...user,
+      token: this.getJwtToken({ id: user.id }),
+    };
+  }
+
   private getJwtToken(payload: JwtPayload) {
     return this.jwtService.sign(payload);
   }
